c9ee219933c0efd2dc9cc7677048230051db5a97
Vinicius Benicio <vinicius.benicio97@gmail.com>
2023-12-06 19:34:46 -0300

    Técnicas e Convenções

---
+++ Store.Tests/Entities/OrderTests.cs
@@ -0,0 +1,99 @@
+﻿namespace Store.Tests.Entities
+{
+    [TestClass]
+    public class OrderTests
+    {
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void Dado_um_novo_pedido_valido_ele_deve_gerar_um_numero_com_8_caracteres()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void Dado_um_novo_pedido_seu_status_deve_ser_aguardando_pagamento()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void Dado_um_pagamento_do_pedido_seu_status_deve_ser_aguardando_entrega()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void Dado_um_pedido_cancelado_seu_status_deve_ser_cancelado()
+        {
+            Assert.Fail();
+        }
+
+
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void Dado_um_novo_item_sem_produto_o_mesmo_nao_deve_ser_adicionado()
+        {
+            Assert.Fail();
+        }
+
+
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void Dado_um_novo_item_com_quantidade_zero_ou_menor_o_mesmo_nao_deve_ser_adicionado()
+        {
+            Assert.Fail();
+        }
+
+
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void Dado_um_novo_pedido_valido_seu_total_deve_ser_50()
+        {
+            Assert.Fail();
+        }
+
+
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void Dado_um_desconto_expirado_o_valor_do_pedido_deve_ser_60()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void Dado_um_desconto_invalido_o_valor_do_pedido_deve_ser_60()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void Dado_um_desconto_de_10_o_valor_do_pedido_deve_ser_50()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void Dado_uma_taxa_de_entrega_de_10_o_valor_do_pedido_deve_ser_60()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void Dado_um_pedido_sem_cliente_o_mesmo_deve_ser_invalido()
+        {
+            Assert.Fail();
+        }
+
+    }
+}
