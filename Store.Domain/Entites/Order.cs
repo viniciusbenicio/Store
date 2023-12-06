20fb12464e1664d3f2999038826fe671299ea5a6
Vinicius Benicio <vinicius.benicio97@gmail.com>
2023-12-05 21:36:58 -0300

    Validando o Pedido

--- Store.Domain/Entites/Order.cs
+++ Store.Domain/Entites/Order.cs
@@ -1,4 +1,5 @@
-﻿using Store.Domain.Enums;
+﻿using Flunt.Validations;
+using Store.Domain.Enums;
 
 namespace Store.Domain.Entites
 {
@@ -6,6 +7,8 @@ namespace Store.Domain.Entites
     {
         public Order(Customer customer, decimal deliveryFee, Discount discount)
         {
+            AddNotifications(new Contract().Requires().IsNotNull(customer, "Customer", "Cliente inválido"));
+
             Customer = customer;
             Date = DateTime.Now;
             Number = Guid.NewGuid().ToString().Substring(0, 8);
@@ -26,7 +29,8 @@ namespace Store.Domain.Entites
         public void AddItem(Product product, int quantity)
         {
             var item = new OrderItem(product, quantity);
-            Items.Add(item);
+            if (item.Valid)
+                Items.Add(item);
         }
 
         public decimal Total()
