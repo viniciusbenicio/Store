c4c53e1bccea534e9a1ecde00aa960cf8631812b
Vinicius Benicio <vinicius.benicio97@gmail.com>
2023-12-06 20:11:48 -0300

    Criando Queries, uma classe static apenas para retonrar uma expressão Ativo ou Inativo.

---
+++ Store.Domain/Queries/ProductQuery.cs
@@ -0,0 +1,19 @@
+﻿using Store.Domain.Entites;
+using System.Linq.Expressions;
+
+namespace Store.Domain.Queries
+{
+    public static class ProductQuery
+    {
+        public static Expression<Func<Product, bool>> GetActiveProducts()
+        {
+            return x => x.Active;
+        }
+
+        public static Expression<Func<Product, bool>> GetInactiveProducts()
+        {
+            return x => x.Active == false;
+        }
+
+    }
+}
