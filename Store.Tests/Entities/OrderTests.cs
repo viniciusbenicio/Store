c02a9cb31ccf8763945b6ca68988b85564e750ba
Vinicius Benicio <vinicius.benicio97@gmail.com>
2023-12-06 20:01:26 -0300

    testes de adição de item, Item invalido, Total do Pedido, Desconto Expirado, Desconto inválido, Desconto valido, Taxa de entrega, um pedido sem cliente.

--- Store.Tests/Entities/OrderTests.cs
+++ Store.Tests/Entities/OrderTests.cs
@@ -56,8 +56,9 @@ namespace Store.Tests.Entities
         [TestCategory("Domain")]
         public void Dado_um_novo_item_sem_produto_o_mesmo_nao_deve_ser_adicionado()
         {
-            Assert.Fail();
-
+            var order = new Order(_customer, 0, null);
+            order.AddItem(null, 0);
+            Assert.AreEqual(order.Items.Count, 0);
         }
 
 
@@ -66,8 +67,9 @@ namespace Store.Tests.Entities
         [TestCategory("Domain")]
         public void Dado_um_novo_item_com_quantidade_zero_ou_menor_o_mesmo_nao_deve_ser_adicionado()
         {
-            Assert.Fail();
-
+            var order = new Order(_customer, 0, null);
+            order.AddItem(_product, 0);
+            Assert.AreEqual(order.Items.Count, 0);
         }
 
 
@@ -76,7 +78,9 @@ namespace Store.Tests.Entities
         [TestCategory("Domain")]
         public void Dado_um_novo_pedido_valido_seu_total_deve_ser_50()
         {
-            Assert.Fail();
+            var order = new Order(_customer, 10, _discount);
+            order.AddItem(_product, 5);
+            Assert.AreEqual(order.Total(), 50);
 
         }
 
@@ -86,7 +90,10 @@ namespace Store.Tests.Entities
         [TestCategory("Domain")]
         public void Dado_um_desconto_expirado_o_valor_do_pedido_deve_ser_60()
         {
-            Assert.Fail();
+            var expiredDiscount = new Discount(10, DateTime.Now.AddDays(-5));
+            var order = new Order(_customer, 10, expiredDiscount);
+            order.AddItem(_product, 5);
+            Assert.AreEqual(order.Total(), 60);
 
         }
 
@@ -94,15 +101,18 @@ namespace Store.Tests.Entities
         [TestCategory("Domain")]
         public void Dado_um_desconto_invalido_o_valor_do_pedido_deve_ser_60()
         {
-            Assert.Fail();
-
+            var order = new Order(_customer, 10, null);
+            order.AddItem(_product, 5);
+            Assert.AreEqual(order.Total(), 60);
         }
 
         [TestMethod]
         [TestCategory("Domain")]
         public void Dado_um_desconto_de_10_o_valor_do_pedido_deve_ser_50()
         {
-            Assert.Fail();
+            var order = new Order(_customer, 10, _discount);
+            order.AddItem(_product, 5);
+            Assert.AreEqual(order.Total(), 50);
 
         }
 
@@ -110,7 +120,9 @@ namespace Store.Tests.Entities
         [TestCategory("Domain")]
         public void Dado_uma_taxa_de_entrega_de_10_o_valor_do_pedido_deve_ser_60()
         {
-            Assert.Fail();
+            var order = new Order(_customer, 10, _discount);
+            order.AddItem(_product, 6);
+            Assert.AreEqual(order.Total(), 60);
 
         }
 
@@ -118,7 +130,8 @@ namespace Store.Tests.Entities
         [TestCategory("Domain")]
         public void Dado_um_pedido_sem_cliente_o_mesmo_deve_ser_invalido()
         {
-            Assert.Fail();
+            var order = new Order(null, 10, _discount);
+            Assert.AreEqual(order.Valid, false);
 
         }
 
