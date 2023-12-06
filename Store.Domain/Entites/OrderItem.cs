d664c812bc2ab69f7a2ac3d820ceedc4ae4aae28
Vinicius Benicio <vinicius.benicio97@gmail.com>
2023-12-05 21:40:40 -0300

    Validando o Item do Pedido

--- Store.Domain/Entites/OrderItem.cs
+++ Store.Domain/Entites/OrderItem.cs
@@ -1,9 +1,16 @@
-﻿namespace Store.Domain.Entites
+﻿using Flunt.Validations;
+
+namespace Store.Domain.Entites
 {
     public class OrderItem : Entity
     {
         public OrderItem(Product product, int quantity)
         {
+            AddNotifications(new Contract().Requires()
+                                           .IsNotNull(product, "Product", "Produto inválido")
+                                           .IsGreaterThan(quantity, 0, "Quantity", "A Quantidade deve ser maior que zero")
+                                           );
+
             Product = product;
             Price = Product != null ? Product.Price : 0;
             Quantity = quantity;
