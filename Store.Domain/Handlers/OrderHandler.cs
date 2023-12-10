8ba4cf340b84fcf05e5fffbd7d2e8ab42bae7891
Vinicius Benicio <vinicius.benicio97@gmail.com>
2023-12-09 21:48:37 -0300

    OrderHandlers e utils para extrair guids e virar uma lista

---
+++ Store.Domain/Handlers/OrderHandler.cs
@@ -0,0 +1,67 @@
+﻿using Flunt.Notifications;
+using Store.Domain.Commands;
+using Store.Domain.Commands.Interfaces;
+using Store.Domain.Entites;
+using Store.Domain.Handlers.Interfaces;
+using Store.Domain.Repositories;
+using Store.Domain.Utils;
+
+namespace Store.Domain.Handlers
+{
+    public class OrderHandler : Notifiable, IHandler<CreateOrderCommand>
+    {
+        private readonly ICustomerRepository _customerRepository;
+        private readonly IDeliveryFeeRepository _deliveryFeeRepository;
+        private readonly IDiscountRepository _discountRepository;
+        private readonly IProductRepository _productRepository;
+        private readonly IOrderRepository _orderRepository;
+
+        public OrderHandler(ICustomerRepository customerRepository, IDeliveryFeeRepository deliveryFeeRepository, IDiscountRepository discountRepository, IProductRepository productRepository, IOrderRepository orderRepository)
+        {
+            _customerRepository = customerRepository;
+            _deliveryFeeRepository = deliveryFeeRepository;
+            _discountRepository = discountRepository;
+            _productRepository = productRepository;
+            _orderRepository = orderRepository;
+        }
+        public ICommandResult Handle(CreateOrderCommand command)
+        {
+            //Fail fast Validation
+
+            command.Validate();
+            if (command.Invalid)
+                return new GenericCommandResult(false, "Pedido inválido", command.Notifications);
+
+
+            // 1. Recuperar o cliente
+            var customer = _customerRepository.Get(command.Customer);
+
+            // 2. Calcula a taxa de entrega
+            var deliveryFee = _deliveryFeeRepository.Get(command.ZipCode);
+
+            // 3. Obtém o cupom de desconto
+            var discount = _discountRepository.Get(command.PromoCode);
+
+            // 4. Gera o pedido
+            var products = _productRepository.Get(ExtractGuids.Extract(command.Items)).ToList();
+            var order = new Order(customer, deliveryFee, discount);
+            foreach (var item in command.Items)
+            {
+                var product = products.Where(x => x.Id == item.Product).FirstOrDefault();
+                order.AddItem(product, item.Quantity);
+            }
+
+            //5 .Agrupa as notificações
+            AddNotifications(order.Notifications);
+
+            //6. Verifica se deu tudo certo
+            if (Invalid)
+                return new GenericCommandResult(false, "Falha ao gerar o pediudo", Notifications);
+
+            //7. Retorna o resultado
+            _orderRepository.Save(order);
+            return new GenericCommandResult(true, $"Pedido {order.Number} gerado com sucesso", order);
+
+        }
+    }
+}
---
+++ Store.Domain/Utils/ExtractGuids.cs
@@ -0,0 +1,18 @@
+﻿using Store.Domain.Commands;
+
+namespace Store.Domain.Utils
+{
+    public static class ExtractGuids
+    {
+        public static IEnumerable<Guid> Extract(IList<CreateOrdemItemCommand> Items)
+        {
+            var guids = new List<Guid>();
+
+            foreach (var item in Items)
+                guids.Add(item.Product);
+
+            return guids;
+
+        }
+    }
+}
